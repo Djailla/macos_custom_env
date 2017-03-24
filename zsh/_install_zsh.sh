35e9c7dbf8182ec96f75be9f192eb454b56ea552
Djailla <bastien.vallet@gmail.com>
2017-03-24 21:44:00 +0100

    Cleanup

--- zsh/_install_zsh.sh
+++ zsh/_install_zsh.sh
@@ -2,7 +2,7 @@
 brew install zsh
 
 # Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 
 # Deploy theme
 cp djailla.zsh-theme ~/.oh-my-zsh/themes/
